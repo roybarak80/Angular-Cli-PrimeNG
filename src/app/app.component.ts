217e22222af5aae8496c15c53a425d5b0befc2bc
roy <barakroy@gmail.com>
2017-08-07 09:55:00 +0300

    BootStrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+        import { Component } from '@angular/core';
+        import {PanelMenuModule, MenuItem} from 'primeng/primeng';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app works!';
-}
+
+        @Component({
+          selector: 'app-root',
+          template: `<button pButton type="button" (click)="onclick()" label="Click"></button>
+          <p-panelMenu [model]="items" [style]="{'width':'300px'}"></p-panelMenu>
+        
+          `,
+        })
+        export class AppComponent {
+          title = 'app works!';
+          items: MenuItem[];
+
+          onclick() {
+            alert(1234);
+          }
+          ngOnInit() {
+            this.items = [
+              {
+                label: 'File',
+                icon: 'fa-file-o',
+                items: [{
+                  label: 'New',
+                  icon: 'fa-plus',
+                  items: [
+                    {label: 'Project'},
+                    {label: 'Other'},
+                  ]
+                },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+                ]
+              },
+              {
+                label: 'Edit',
+                icon: 'fa-edit',
+                items: [
+                  {label: 'Undo', icon: 'fa-mail-forward'},
+                  {label: 'Redo', icon: 'fa-mail-reply'}
+                ]
+              },
+              {
+                label: 'Help',
+                icon: 'fa-question',
+                items: [
+                  {
+                    label: 'Contents'
+                  },
+                  {
+                    label: 'Search',
+                    icon: 'fa-search',
+                    items: [
+                      {
+                        label: 'Text',
+                        items: [
+                          {
+                            label: 'Workspace'
+                          }
+                        ]
+                      },
+                      {
+                        label: 'File'
+                      }
+                    ]}
+                ]
+              },
+              {
+                label: 'Actions',
+                icon: 'fa-gear',
+                items: [
+                  {
+                    label: 'Edit',
+                    icon: 'fa-refresh',
+                    items: [
+                      {label: 'Save', icon: 'fa-save'},
+                      {label: 'Update', icon: 'fa-save'},
+                    ]
+                  },
+                  {
+                    label: 'Other',
+                    icon: 'fa-phone',
+                    items: [
+                      {label: 'Delete', icon: 'fa-minus'}
+                    ]
+                  }
+                ]
+              }
+            ];
+          }
+
+        }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+        import { BrowserModule } from '@angular/platform-browser';
+        import { NgModule } from '@angular/core';
+        import { FormsModule } from '@angular/forms';
+        import { CommonModule } from '@angular/common';
+        import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+        import { HttpModule } from '@angular/http';
+        import {ButtonModule} from 'primeng/primeng';
+        import { RouterModule } from '@angular/router';
+        import {PanelMenuModule} from 'primeng/primeng';
+        import { AppComponent } from './app.component';
 
-import { AppComponent } from './app.component';
+        @NgModule({
+          declarations: [
+            AppComponent
+          ],
+          imports: [
+            BrowserAnimationsModule,
+            BrowserModule,
+            FormsModule,
+            HttpModule,
+            ButtonModule,
+            CommonModule,
+            RouterModule,
+            PanelMenuModule,
+          ],
+          providers: [],
+          bootstrap: [AppComponent]
+        })
+        export class AppModule {
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+        }
